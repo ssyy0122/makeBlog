0c1dcca35531c34ec251e0162f03591b18e5aa3c
ssyy0122 <z01039118740@gmail.com>
2022-05-11 23:48:09 +0900

    controller make

--- src/main/java/com/victolee/board/controller/BoardController.java
+++ src/main/java/com/victolee/board/controller/BoardController.java
@@ -1,4 +1,30 @@
 package com.victolee.board.controller;
 
+import com.victolee.board.dto.BoardDto;
+import com.victolee.board.service.BoardService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+@Controller// 컨트롤러라는걸 명시함
+@AllArgsConstructor
+
 public class BoardController {
+    private BoardController boardController;
+
+        @GetMapping("/")
+        public String list() {
+            return "board/list.html";
+        }
+        @GetMapping("/post")
+        public String write() {
+            return  "board/write.html";
+        }
+
+        @PostMapping("/post")
+        public String write(BoardDto boardDto) {
+            boardService.savePost(boardDto);
+            return "redirect:/";
+
+        }
 }
