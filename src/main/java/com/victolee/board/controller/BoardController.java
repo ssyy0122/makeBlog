34c3a75d74ebcfaebcf87def88882546f9feb2e5
ssyy0122 <z01039118740@gmail.com>
2022-05-12 23:44:41 +0900

    test

--- src/main/java/com/victolee/board/controller/BoardController.java
+++ src/main/java/com/victolee/board/controller/BoardController.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+
 @Controller// 컨트롤러라는걸 명시함
 @AllArgsConstructor
 
---
+++ src/main/java/com/victolee/board/service/BoardService.java
@@ -0,0 +1,12 @@
+package com.victolee.board.service;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor //레파지토리 주입
+@Service //서비스 계층이라고 알려줌
+
+public class BoardService {
+
+}
