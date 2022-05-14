207660daf1825e6db0573e8e93639bdc5fbf4de6
ssyy0122 <z01039118740@gmail.com>
2022-05-14 19:06:46 +0900

    Applicatin file add

--- src/main/java/com/victolee/board/BoardApplication.java
+++ src/main/java/com/victolee/board/BoardApplication.java
@@ -1,4 +1,13 @@
 package com.victolee.board;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
 public class BoardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BoardApplication.class ,args);
+    }
 }
