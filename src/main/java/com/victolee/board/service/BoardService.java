349f5db514550cb496c30ada6577758f815acf0a
ssyy0122 <z01039118740@gmail.com>
2022-05-16 23:01:49 +0900

    service add

--- src/main/java/com/victolee/board/service/BoardService.java
+++ src/main/java/com/victolee/board/service/BoardService.java
@@ -1,36 +1,40 @@
 package com.victolee.board.service;
 
-
 import com.victolee.board.domain.entity.BoardEntity;
-import com.victolee.board.dto.BoardDto;
 import com.victolee.board.domain.repository.BoardRepository;
+import com.victolee.board.dto.BoardDto;
 import lombok.AllArgsConstructor;
+import lombok.Setter;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Optional;
 
-@AllArgsConstructor //레파지토리 주입
-@Service //서비스 계층이라고 알려줌
+@AllArgsConstructor
+@Service
 public class BoardService {
     private BoardRepository boardRepository;
 
     @Transactional
-    public List<BoardDto> getBoardlist(){
-        List<BoardEntity>boardEntities = boardRepository.findAll();
-        List<BoardDto> boardDtoList = new ArrayList<>();
+    public BoardDto getPost(Long id){
+        Optional<BoardEntity>boardEntityWrapper = boardRepository.findById(id);
+        BoardEntity boardEntity = boardEntityWrapper.get();
 
-        for (BoardEntity boardEntity : boardEntities) {
-            BoardDto boardDTO = BoardDto.builder()
-                    .id(boardEntity.getId())
-                    .title(boardEntity.getTitle())
-                    .content(boardEntity.getContent())
-                    .writer(boardEntity.getWriter())
-                    .createdDate(boardEntity.getCreatedDate())
-                    .build();
-            boardDtoList.add(boardDTO);
-        }
-        return boardDtoList;
+        BoardDto boardDto = BoardDto.builder()
+                .id(boardEntity.getId())
+                .title(boardEntity.getTitle())
+                .content(boardEntity.getContent())
+                .writer(boardEntity.getWriter())
+                .createdDate(boardEntity.getCreatedDate())
+                .build();
+        return boardDto;
+    }
+    @Transactional
+    public Long savePost(BoardDto boardDto){
+        return boardRepository.save(boardDto.toEntity()).getId();
+    }
+    @Transactional
+    public void deletePost(Long id) {
+        boardRepository.deleteById(id);
     }
 }
