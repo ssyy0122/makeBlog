233a1a6c8e2dd26569c61966edbd9d0a56fd83b5
ssyy0122 <z01039118740@gmail.com>
2022-05-16 10:24:33 +0900

    service 구현

--- src/main/java/com/victolee/board/service/BoardService.java
+++ src/main/java/com/victolee/board/service/BoardService.java
@@ -1,12 +1,15 @@
 package com.victolee.board.service;
 
 
+import com.victolee.board.domain.entity.BoardEntity;
 import com.victolee.board.dto.BoardDto;
 import com.victolee.board.domain.repository.BoardRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor //레파지토리 주입
 @Service //서비스 계층이라고 알려줌
@@ -14,7 +17,20 @@ public class BoardService {
     private BoardRepository boardRepository;
 
     @Transactional
-    public Long savePost(BoardDto boardDto) {
-        return boardRepository.save(boardDto.toEntity()).getId();
+    public List<BoardDto> getBoardlist(){
+        List<BoardEntity>boardEntities = boardRepository.findAll();
+        List<BoardDto> boardDtoList = new ArrayList<>();
+
+        for (BoardEntity boardEntity : boardEntities) {
+            BoardDto boardDTO = BoardDto.builder()
+                    .id(boardEntity.getId())
+                    .title(boardEntity.getTitle())
+                    .content(boardEntity.getContent())
+                    .writer(boardEntity.getWriter())
+                    .createdDate(boardEntity.getCreatedDate())
+                    .build();
+            boardDtoList.add(boardDTO);
+        }
+        return boardDtoList;
     }
 }
